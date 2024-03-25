e0060c5cff56004a7505318b2d040ed2c1c3b20e
Igor Swatowski <igorswatowski@gmail.com>
2024-03-25 15:09:56 +0100

    Change logo src

--- src/components/Sidebar/MountingPoint.tsx
+++ src/components/Sidebar/MountingPoint.tsx
@@ -1,14 +1,12 @@
+import logo from "@assets/logo.png";
 import { Box, Text } from "@saleor/macaw-ui/next";
 import React from "react";
 
 export const MountingPoint = () => {
   return (
     <Box display="flex" gap={3} paddingX={4} paddingY={5} alignItems="center">
-      <Box height={8} width={8}>
-        <img
-          src="https://salingo-test-media.s3.eu-central-1.amazonaws.com/thumbnails/app-brand-data/logo_63da7a77_thumbnail_64.webp"
-          alt="logo"
-        />
+      <Box height={12} width={12}>
+        <img src={logo} alt="logo" />
       </Box>
       <Text variant="bodyStrong" size="small">
         Retrader Dashboard
